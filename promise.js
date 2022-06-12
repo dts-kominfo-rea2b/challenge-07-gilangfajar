8625d3dd58a69d6b449f54ac6f42519fe51c5fc0
Gilang Fajar <49382999+gilangfajar@users.noreply.github.com>
2022-06-12 08:34:07 +0700

    modified promise.js

--- promise.js
+++ promise.js
@@ -1,7 +1,14 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try {
+    const [totalIXX, totalVGC] = await Promise.all([promiseTheaterIXX(), promiseTheaterVGC()]);
+    return [...totalIXX, ...totalVGC].filter((x) => x.hasil === emosi).length;
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 module.exports = {
   promiseOutput,
